0dc406a590516f2efa3af1e15d7138d1fa1e619f
CompSci2013 <kcollins2004@gmail.com>
2021-02-20 22:10:34 +0000

    Add logic and test

---
+++ src/main/java/com/lab/pipeline/continuous/continuouspipelinedemo/ContinuousPipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lab.pipeline.continuous.continuouspipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousPipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousPipelineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/pipeline/continuous/continuouspipelinedemo/ContinuousPipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.pipeline.continuous.continuouspipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuousPipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
